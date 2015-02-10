fa03bcf2a021156978aae1a9707601c1c1b6dfbc
Hackbright Student <info@hackbrightacademy.com>
2015-02-09 18:05:12 -0800

    first commit! Wrote script to seed the DB and built class model for all tables

--- model.py
+++ model.py
@@ -1,17 +1,77 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker
 
+ENGINE = None
+Session = None
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+    def __repr__(self):
+        """Show info about the Python User object"""
+
+        return "<User id: %d, email: %s, password: %s, age: %d, zipcode: %s>" % (self.id, self.email, self.password, self.age, self.zipcode)
+
+
+
+class Movie(Base):
+
+    __tablename__ = "Movies"
+    
+    id = Column(Integer, primary_key = True)
+    movie_title = Column(String(64), nullable=True)
+    release_date = Column(String(10), nullable=True)
+    IMDb_url = Column(String(64), nullable=True)
+
+    def __repr__(self):
+        """Show info about the Python Movie object"""
+
+        return "<Movie id: %d, movie_title: %s, release_date: %s, IMDb_url: %s>" % (self.id, self.movie_title, self.release_date, self.IMDb_url)
+
+
+
+class Rating(Base):
+
+    __tablename__ = "Ratings"
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey("Users.id"), nullable=False)
+    movie_id = Column(Integer, ForeignKey("Movies.id"), nullable=False)
+    rating = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        """Show info about the Python Rating object"""
+
+        return "<user_id: %d, movie_id: %d, rating(0-5): %d>" % (self.user_id, self.movie_id, self.rating)
 
 
 ### End class declarations
 
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
+
 def main():
     """In case we need this for something"""
     pass
+
 
 if __name__ == "__main__":
     main()
--- seed.py
+++ seed.py
@@ -2,20 +2,71 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    
+    user_file = open("./seed_data/u.user")
+
+    for line in user_file:
+        line_list = line.split("|")
+        user_object = model.User()
+
+        # This pulls attributes from the line of raw data & assigns to object
+        user_object.id = int(line_list[0])
+        user_object.age = int(line_list[1])
+        user_object.zipcode = line_list[4]
+        session.add(user_object)
+
+    session.commit()
+
 
 def load_movies(session):
-    # use u.item
-    pass
+
+    movies_file = open("./seed_data/u.item")
+
+    for line in movies_file:
+        line_list = line.split("|")
+        movie_object = model.Movie()
+
+        # this pulls attributes from the line of raw data & assigned to object
+        movie_object.id = int(line_list[0])
+        # This cleans up the title and strips release date from the title string
+        title = line_list[1]
+        title = title.decode("latin-1")
+        movie_object.movie_title = title[:-7]
+
+        movie_object.release_date = line_list[2]
+        movie_object.IMDb_url = line_list[4]
+        session.add(movie_object)
+
+    session.commit()
+
+
 
 def load_ratings(session):
-    # use u.data
-    pass
+    
+    ratings_file = open("./seed_data/u.data")
+
+    for line in ratings_file:
+        rating_list = line.split()
+        a_rating = model.Rating()
+
+        a_rating.user_id = int(rating_list[0])
+        a_rating.movie_id = int(rating_list[1])
+        a_rating.rating = int(rating_list[2])
+
+        session.add(a_rating)
+
+    session.commit() 
+
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    # load_users(session)
+    # load_movies(session)
+    load_ratings(session)
+
+
+
 
 if __name__ == "__main__":
     s= model.connect()
